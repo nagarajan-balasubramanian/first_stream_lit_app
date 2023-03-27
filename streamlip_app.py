32effa3bbc64925caf29b3af894af4281b5b3412
nagarajan-balasubramanian <58990975+nagarajan-balasubramanian@users.noreply.github.com>
2023-03-27 11:32:22 +0100

    Update streamlip_app.py

--- streamlip_app.py
+++ streamlip_app.py
@@ -1,1 +1,9 @@
+import streamlit
+
+streamlit.title('My First Hotel Menu by Streamlit')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
 print('Streamlit App')
